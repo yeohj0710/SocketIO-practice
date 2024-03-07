9a49ac61e3c1f9fa9dd631bc9c073e863efb9089
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-03-08 00:03:12 +0900

    2403081203

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hi");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3333 ✅`);
+app.listen(3333, handleListen);
